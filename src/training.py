b3e9b1556e79b2cfff854a57ee1b0c66edaca7e2
f2015712 <sks2971996@gmail.com>
2024-12-10 14:18:18 +0530

    resolve bug

--- src/training.py
+++ src/training.py
@@ -23,7 +23,7 @@ y_pred = model.predict(X_test)
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
-  print(f"Mean Squared Error: {mse}")
+print(f"Mean Squared Error: {mse}")
 print(f"R-squared: {r2}")
 
 # Save the model
